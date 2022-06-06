8a98392720d929b9df3681acabfd4c1ca7f335c9
Niels Hofmans <niels@ironpeak.be>
2022-06-06 23:26:49 +0200

    chore(maclaunch): brew audit recommendations

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -1,10 +1,10 @@
 class Maclaunch < Formula
-  desc "Manage your macOS startup items."
+  desc "Manage your macOS startup items"
   homepage "https://github.com/hazcod/maclaunch"
-  url "https://codeload.github.com/hazcod/maclaunch/zip/2.3.1"
-  version "2.3.1"
-  sha256 "579394e1cf97cfd3a321c5b0278d34917e0365d5e88a836264abad4798c754b8"
   license "MIT"
+  
+  url "https://github.com/hazcod/maclaunch/archive/2.3.1.tar.gz"
+  sha256 "579394e1cf97cfd3a321c5b0278d34917e0365d5e88a836264abad4798c754b8"
 
   def install
     bin.install "maclaunch.sh" => "maclaunch"
