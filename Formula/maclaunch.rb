3bf29f6f35d1038e46559edcb30cf0d30ec1860b
Niels Hofmans <niels@ironpeak.be>
2022-06-06 23:24:57 +0200

    chore(maclaunch): set license

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -4,6 +4,7 @@ class Maclaunch < Formula
   url "https://codeload.github.com/hazcod/maclaunch/zip/2.3.1"
   version "2.3.1"
   sha256 "579394e1cf97cfd3a321c5b0278d34917e0365d5e88a836264abad4798c754b8"
+  license "MIT"
 
   def install
     bin.install "maclaunch.sh" => "maclaunch"
