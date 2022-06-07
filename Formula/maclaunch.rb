af7f2d9cec41ffa12bb00cf02a48a104b552a91c
Niels Hofmans <niels@ironpeak.be>
2022-06-07 11:08:05 +0200

    chore(maclaunch): add depends_on

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -4,6 +4,7 @@ class Maclaunch < Formula
   url "https://github.com/hazcod/maclaunch/archive/2.3.1.tar.gz"
   sha256 "abba1f7cffd7f694b23745f6ccc137b17b6c9ea38fe2fbb55a8bd9646f6ae1a1"
   license "MIT"
+  depends_on :macos
 
   def install
     bin.install "maclaunch.sh" => "maclaunch"
