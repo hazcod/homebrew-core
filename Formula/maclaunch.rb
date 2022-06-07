a5b6cfce66901c9ea33e99bcb3f4b2b544efcae8
Niels Hofmans <niels@ironpeak.be>
2022-06-07 08:07:02 +0200

    chore(maclaunch): formula ordering

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -1,10 +1,9 @@
 class Maclaunch < Formula
   desc "Manage your macOS startup items"
   homepage "https://github.com/hazcod/maclaunch"
-  license "MIT"
-  
   url "https://github.com/hazcod/maclaunch/archive/2.3.1.tar.gz"
   sha256 "579394e1cf97cfd3a321c5b0278d34917e0365d5e88a836264abad4798c754b8"
+  license "MIT"
 
   def install
     bin.install "maclaunch.sh" => "maclaunch"
