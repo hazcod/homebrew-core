825322370509eda2d337d7f96d1e805fe7e519a2
Niels Hofmans <niels@ironpeak.be>
2022-11-15 09:30:55 +0100

    update(maclaunch): update to 2.4
    This fixes Ventura loginItem and disabled detection.

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -1,9 +1,13 @@
 class Maclaunch < Formula
   desc "Manage your macOS startup items"
   homepage "https://github.com/hazcod/maclaunch"
-  url "https://github.com/hazcod/maclaunch/archive/2.3.1.tar.gz"
-  sha256 "abba1f7cffd7f694b23745f6ccc137b17b6c9ea38fe2fbb55a8bd9646f6ae1a1"
+  url "https://github.com/hazcod/maclaunch/archive/2.4.tar.gz"
+  sha256 "9ae98a3bf592f002d2235f240c4c3318551cb17cdf1680ad060000fd69e11bf9"
   license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "99d01d6f2421a1178744e66ca594636adb8d4e7ae6efcd9f3f688be215667977"
+  end
   depends_on :macos
 
   def install
